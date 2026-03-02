0cfad52046b9a77d2da1a556a9923526c392cbe4
VuBui217 <vubui217@gmail.com>
2026-03-01 22:58:55 -0500

    Fix swapped hints in check_guess function and streamline secret assignment

--- app.py
+++ app.py
@@ -34,10 +34,11 @@ def check_guess(guess, secret):
         return "Win", "🎉 Correct!"
 
     try:
+        # FIXME: Hints are swapped
         if guess > secret:
-            return "Too High", "📈 Go HIGHER!"
+            return "Too High", "📉 Go LOWER!" 
         else:
-            return "Too Low", "📉 Go LOWER!"
+            return "Too Low", "📈 Go HIGHER!"
     except TypeError:
         g = str(guess)
         if g == secret:
@@ -156,7 +157,7 @@ if submit:
         st.session_state.history.append(guess_int)
 
         if st.session_state.attempts % 2 == 0:
-            secret = str(st.session_state.secret)
+            secret = st.session_state.secret
         else:
             secret = st.session_state.secret
 
